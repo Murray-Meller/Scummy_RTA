348bef585e658f163f17cbc8314e11fd2d02da2e
dhar3454 <dhar3454@uni.sydney.edu.au>
2017-10-09 06:19:56 +1100

    Backend functional

---
+++ backend.py
@@ -0,0 +1,190 @@
+from bottle import run, request, post, get
+
+# Important globals
+host = "localhost"
+port = "8082"
+
+
+# Our "Database"
+users = []
+vehicles = []
+destroyed_vehicles = []
+
+user_database = './database/users.txt'
+vehicle_database = './database/vehicles.txt'
+destroyed_vehicle_database = './database/destroyed_vehicles.txt'
+
+user_database_num_fields = 6
+vehicle_database_num_fields = 4
+destroyed_vehicle_database_num_fields = 1
+
+def load_users():
+    udb = open(user_database, 'r')
+    for line in udb:
+        line = line.split(',')
+        line[-1] = line[-1][:-1]
+        users.append(line)
+    udb.close()
+    return users
+
+def save_users():
+    udb = open(user_database, 'w')
+    for user in users:
+        count = 0
+        for field in user:
+            udb.write(str(field))
+            count += 1
+            if count != user_database_num_fields:
+                udb.write(',')
+        for i in range(user_database_num_fields - (count + 1)):
+            udb.write(',')
+        udb.write('\n')
+    udb.close()
+
+def load_vehicles():
+    vdb = open(vehicle_database, 'r')
+    for line in vdb:
+        line = line.split(',')
+        line[-1] = line[-1][:-1]
+        vehicles.append(line)
+    vdb.close()
+
+    return vehicles
+
+def save_vehicles():
+    vdb = open(vehicle_database, 'w')
+    for vehicle in vehicles:
+        count = 0
+        for field in vehicle:
+            vdb.write(str(field))
+            count += 1
+            if count != vehicle_database_num_fields:
+                vdb.write(',')
+        for i in range(vehicle_database_num_fields - (count + 1)):
+            vdb.write(',')
+        vdb.write('\n')
+    vdb.close()
+
+def load_destroyed_vehicles():
+    dvdb = open(destroyed_vehicle_database, 'r')
+    for line in dvdb:
+        line = line.split(',')
+        line[-1] = line[-1][:-1]
+        destroyed_vehicles.append(line)
+    dvdb.close()
+    return destroyed_vehicles
+
+def save_destroyed_vehicles():
+    dvdb = open(destroyed_vehicle_database, 'w')
+    for vehicle in destroyed_vehicles:
+        count = 0
+        for field in vehicle:
+            dvdb.write(str(field))
+            count += 1
+            if count != destroyed_vehicle_database_num_fields:
+                dvdb.write(',')
+        for i in range(destroyed_vehicle_database_num_fields - (count + 1)):
+            dvdb.write(',')
+        dvdb.write('\n')
+    dvdb.close()
+
+def load_database():
+    load_users()
+    load_vehicles()
+    load_destroyed_vehicles()
+
+def save_database():
+    save_users()
+    save_vehicles()
+    save_destroyed_vehicles()
+
+def admin_reset():
+    dvdb = open(user_database, 'w')
+    dvdb.write("ID,NAME,HASHED PASSWORD,LEVEL,LICENSE,LICENSE_EXPIRY\n")
+    dvdb.close()
+    dvdb = open(vehicle_database, 'w')
+    dvdb.write("USER_ID,VEHICLE_ID,FINES,DEMERITS\n")
+    dvdb.close()
+    dvdb = open(destroyed_vehicle_database, 'w')
+    dvdb.write("VEHICLE_ID")
+    dvdb.close()
+
+# API calls
+@post('/api/useradd/<username:path>/<password:path>/<persontype:path>')
+def useradd(username, password, persontype):
+	load_users()
+	global users
+	for user in users:
+		if user[1] == username:
+			return "User already exists"
+	users.append([len(users),username,password,persontype,"",""])
+	save_users()
+	return "User added"
+
+@post('/api/userget/<username:path>')
+def userget(username):
+	load_users()
+	for user in users:
+		if user[1] == username:
+			r_string = ''
+			for field in user:
+				r_string += field + ','
+			return r_string[:-1]
+	return "User does not exist!"
+
+@post('/api/userget/all')
+def usergetall():
+	r_string = ''
+	udb = open(user_database, 'r')
+	count = 0
+	for user in udb:
+		count += 1
+		if count != 1:
+			r_string += user
+	return r_string
+
+@post('/api/vehicleadd/<vehicle:path>')
+def vehicleadd(vehicle):
+	load_vehicles()
+	global vehicles
+	vehicles.append(vehicle)
+	save_vehicles()
+	return "User added"
+
+@post('/api/vehicleget/all')
+def vehiclegetall():
+	load_vehicles()
+	r_string = ''
+	cdb = open(vehicle_database, 'r')
+	count = 0
+	for car in cdb:
+		count += 1
+		if count != 1:
+			r_string += car
+	return r_string
+
+# TODO: currently no way to add destroyed car in frontend
+# @post('/api/destroyedadd/<destroyed:path>')
+# def destroyedadd(destroyed):
+# 	load_destroyed_vehicles()
+# 	global destroyed_vehicles
+# 	users.append([destroyed_vehicle])
+# 	save_users()
+# 	return "User added"
+
+@post('/api/destroyedget/all')
+def destroyedgetall():
+	r_string = ''
+	cdb = open(destroyed_vehicle_database, 'r')
+	count = 0
+	for car in cdb:
+		count += 1
+		if count != 1:
+			r_string += car
+	return r_string
+
+# Rather than using paths, you could throw all the requests with form data filled using the
+# requests module and extract it here.
+
+# Run the server
+run(host=host, port=port)
--- backend/api_implementation/frontend.py
+++ backend/api_implementation/frontend.py
@@ -3,7 +3,7 @@ import requests
 
 host_addr = "localhost"
 frontend_port = 8080
-backend_port = 8081
+backend_port = 8082
 
 backend_str = "http://{host}:{port}".format(host=host_addr, port=backend_port)
 
@@ -14,6 +14,7 @@ def useradd():
         <form action="/" method="post">
             Username: <input name="username" type="text" />
             Password: <input name="password" type="password" />
+            Type:     <input name="persontype" type="text" />
             <input value="Login" type="submit" />
         </form>
     '''
@@ -22,14 +23,27 @@ def useradd():
 def useradd():
     username = request.forms.get("username")
     password = request.forms.get("password")
-    
+    persontype = request.forms.get("persontype")
+
     # Call the back-end
-    response = requests.post("{target}/api/useradd/{username}/{password}"
-    	.format(target=backend_str, username=username, password=password))
-    
+    response = requests.post("{target}/api/useradd/{username}/{password}/{persontype}"
+    	.format(target=backend_str, username=username, password=password, persontype=persontype))
+
     # Tell the user what the back-end thinks
     # Alternatively, reformat what the back-end thinks into a nicer page
     return response.text
 
+@get('/test1')
+def test1get():
+    response = requests.post("{target}/api/userget/all"
+    	.format(target=backend_str))
+    print(response.text)
+    return response.text
+
+@get('/test2')
+def test2get():
+    response = requests.post("{target}/api/userget/d@d"
+    	.format(target=backend_str))
+    return response.text
 
 run(host=host_addr, port=frontend_port)
